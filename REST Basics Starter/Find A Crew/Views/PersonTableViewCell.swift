3b67eb6d4931e320a1753b1c92e99f74e9962769
AustinPotts <pottsevolvements@gmail.com>
2019-10-01 12:04:39 -0400

    Configured Project

---
+++ REST Basics Starter/Find A Crew/Views/PersonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonTableViewCell.swift
+//  Unit 2 - REST Basics
+//
+//  Created by Spencer Curtis on 5/15/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+    
+    
+    
+}
